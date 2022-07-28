f4d12b8726987576b43b097c98c97c810dae7a34
Fedor Goloshchapov <fedor.goloshchapov@siemens.com>
2022-07-28 17:50:53 +0300

    initial commit

--- src/test/kotlin/fdr/home/task/database/PostgresContainerWrapper.kt
+++ src/test/kotlin/fdr/home/task/database/PostgresContainerWrapper.kt
@@ -1,5 +1,8 @@
 package fdr.home.task.database
 
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.utility.DockerImageName
+
 class PostgresContainerWrapper {
     val container = PostgreSQLContainer<Nothing>(
         DockerImageName.parse("postgres:13-alpine")
