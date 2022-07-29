ebb88e93b3cbdbda8f1b895a5b06e9cc3f6ac309
Fedor Goloshchapov <fedor.goloshchapov@siemens.com>
2022-07-29 15:11:55 +0300

    commit

---
+++ src/main/kotlin/fdr/home/task/config/Configuration.kt
@@ -0,0 +1,14 @@
+package fdr.home.task.config
+
+import fdr.home.task.user.storage.PostgresUserStorage
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
+
+@Configuration
+class Configuration {
+    @Bean
+    fun getPostgresUserStorage(jdbcTemplate: JdbcTemplate): PostgresUserStorage {
+        return PostgresUserStorage(jdbcTemplate)
+    }
+}
